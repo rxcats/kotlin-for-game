3813e5eaae2aae9e35767d28d96e2d73a95335dc
rxcats <windpyj80@gmail.com>
2023-11-21 11:28:37 +0900

    gradle upgrade to 8.4, add createModule task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import io.spring.gradle.dependencymanagement.dsl.ImportsHandler
 import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.incremental.createDirectory
 
 fun ImportsHandler.mavenBom(dep: Provider<MinimalExternalModuleDependency>) = this.mavenBom(dep.get().toString())
 fun PluginAware.apply(dep: Provider<PluginDependency>) = this.apply(plugin = dep.get().pluginId)
@@ -60,5 +61,38 @@ subprojects {
 
     tasks.withType<Delete> {
         delete("build", "out")
+    }
+}
+
+// Task for creating module directories
+// After creating the directory, you need to add the module information to `settings.gradle.kts`
+tasks.register("createModule") {
+    doLast {
+        val moduleName = providers.systemProperty("moduleName").orNull
+
+        if (moduleName.isNullOrBlank()) {
+            error("require moduleName property `-DmoduleName={moduleName}`")
+        }
+
+        // main
+        file("${rootDir.absolutePath}/${moduleName}/src/main/kotlin/io/github/rxcats").createDirectory()
+        file("${rootDir.absolutePath}/${moduleName}/src/main/resources").createDirectory()
+        file("${rootDir.absolutePath}/${moduleName}/src/main/resources/application.properties").createNewFile()
+
+        // test
+        file("${rootDir.absolutePath}/${moduleName}/src/test/kotlin/io/github/rxcats").createDirectory()
+        file("${rootDir.absolutePath}/${moduleName}/src/test/resources").createDirectory()
+        file("${rootDir.absolutePath}/${moduleName}/src/test/resources/application.properties").createNewFile()
+
+        // build.gradle.kts
+        val gradleBuildDefaultDeps = """
+            dependencies {
+                implementation(libs.spring.boot.starter)
+                testImplementation(libs.spring.boot.starter.test)
+            }
+        """.trimIndent()
+        file("${rootDir.absolutePath}/${moduleName}/build.gradle.kts").writeText(gradleBuildDefaultDeps, Charsets.UTF_8)
+
+        println("""##### After add `include(":$moduleName")` to `settings.gradle.kts` #####""")
     }
 }
